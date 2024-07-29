2c538774bbe82b0116cf1b0da62dd15cf6e96686
Jannes Kordilla <jkordil_idaea@MacBook-Pro-von-Jannes.local>
2024-07-29 13:00:57 +0200

    Logger now saves log files in subfolder of current main file. Logs are appendend if file already exists.

--- src/openkarst/utils/logging_config.py
+++ src/openkarst/utils/logging_config.py
@@ -11,13 +11,27 @@ import os
 import logging
 
 def setup_logging():
-    log_dir = 'logs'
-    os.makedirs(log_dir, exist_ok=True)
+    """
+    Sets up logging for the simulation. Logs are saved in a 'logs' folder located in the same
+    directory as the main script.
 
+    This function configures the logging module to log messages to a file named 'simulation.log'
+    within a 'logs' directory. If the 'logs' directory does not exist, it is created. The log file
+    is overwritten each time the function is called.
+
+    The log format includes the timestamp, logger name, log level, and the log message.
+
+    Returns:
+        logging.Logger: Configured logger for the simulation.
+    """
+    current_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current script
+    log_dir = os.path.join(current_dir, 'logs')
+    os.makedirs(log_dir, exist_ok=True)
+    
     log_path = os.path.join(log_dir, 'simulation.log')
     logging.basicConfig(
         filename=log_path,
-        filemode='w',
+        filemode='a',
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO,
         force=True
