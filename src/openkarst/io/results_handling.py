37454aff9aa6d20fc48b4f10cd7512522a52f566
Jannes Kordilla <jkordil_idaea@MacBook-Pro-von-Jannes.local>
2024-07-29 22:40:27 +0200

    Added docstrings

--- src/openkarst/io/results_handling.py
+++ src/openkarst/io/results_handling.py
@@ -11,11 +11,45 @@ import numpy as np
 from typing import Dict
 
 def initialize_results_container(desired_outputs: Dict[str, bool], logger):
+    """
+    Initializes a results container based on the desired outputs.
+
+    This function creates a dictionary to store simulation results for the keys 
+    specified in `desired_outputs` that are set to True. It logs the creation of 
+    the results container using the provided logger.
+
+    Args:
+        desired_outputs (Dict[str, bool]): A dictionary specifying which results to store. 
+            Keys are the names of the results, and values are booleans indicating whether 
+            to store that result.
+        logger (logging.Logger): Logger instance for logging the creation of the results container.
+
+    Returns:
+        Dict[str, list]: A dictionary initialized to store lists of the specified results.
+    """
+    
     results_container = {key: [] for key in desired_outputs if desired_outputs[key]}
     logger.info('Results container created for: %s', desired_outputs)
     return results_container
 
 def store_results(simulation_instance, results_container):
+    """
+    Stores the current state of the simulation in the results container.
+
+    This function appends the current state of various simulation variables to the 
+    corresponding lists in the results container. The variables to store are specified 
+    by the keys of the results container.
+
+    Args:
+        simulation_instance: An instance of the simulation class containing the current 
+            state of the simulation variables.
+        results_container (Dict[str, list]): A dictionary containing lists to store the 
+            simulation results.
+
+    Returns:
+        Dict[str, list]: The updated results container with the current simulation state appended.
+    """
+    
     if 'convergence_fails' in results_container:
         results_container['convergence_fails'].append(simulation_instance.convergence_fails)
     if 'flowrates' in results_container:
