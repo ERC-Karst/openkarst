89721223aec55149c41fe0101272a78129ed612c
Jannes Kordilla <jkordil_idaea@MacBook-Pro-von-Jannes.local>
2024-08-19 14:17:39 +0200

    Added export of static inflow and outflow nodes to VTK exporter

--- src/openkarst/io/vtk_data_exporter.py
+++ src/openkarst/io/vtk_data_exporter.py
@@ -16,14 +16,16 @@ class VtkDataExporter:
     A class to export simulation results to VTK files.
 
     This class takes in a network geometry, flow rate history, water depth history,
-    and time history, and exports the results to VTK files for visualization in ParaView.
+    and time history, optionally inflow and outflow node indices, and exports the results to
+    VTK files for visualization in ParaView.
 
     Attributes:
         output_dir (str): Directory where VTK files will be saved.
 
     Methods:
-        export(cn_geometry, Q_history, y_history, t_history): Exports the simulation
-            results to VTK files.
+        export(cn_geometry, Q_history, y_history, t_history, inflow_nodes=None, outflow_nodes=None):
+            Exports the simulation results to VTK files. If inflow or outflow nodes are specified,
+            it also exports a static VTK file with inflow and outflow node markers.
     """
     
     def __init__(self, output_dir: str):
@@ -45,18 +47,24 @@ class VtkDataExporter:
         name = re.sub(r'[^0-9a-zA-Z_]', '_', name)
         return name
 
-    def export(self, cn_geometry, Q_history, y_history, t_history):
+    def export(self, cn_geometry, Q_history, y_history, t_history, inflow_nodes=None, outflow_nodes=None):
         """
         Exports the simulation results to VTK files.
 
         Args:
-            cn_geometry (openpnm.network.GenericNetwork): The network geometry.
+            cn_geometry (openpnm.network): The network geometry.
             Q_history (list): Flow rate history for each edge.
             y_history (list): Water depth history for each node.
             t_history (list): Time history for the simulation.
+            inflow_nodes (list, optional): List of inflow node indices. Defaults to None.
+            outflow_nodes (list, optional): List of outflow node indices. Defaults to None.
         """
         points = cn_geometry['pore.coords']
         connections = cn_geometry['throat.conns']
+        
+        # Save static node markers if inflow or outflow nodes are provided
+        if inflow_nodes is not None or outflow_nodes is not None:
+            self.export_static_node_markers(cn_geometry, inflow_nodes, outflow_nodes)
         
         for i, (Q, y, t) in enumerate(zip(Q_history, y_history, t_history)):
             polydata = pv.PolyData()
@@ -65,6 +73,7 @@ class VtkDataExporter:
             polydata.lines = lines
             polydata.cell_data[self.vtk_safe('Flowrate')] = Q
             polydata.point_data[self.vtk_safe('WaterDepth')] = y
+        
             if 'throat.epsilon' in cn_geometry:
                 polydata.cell_data[self.vtk_safe('Epsilon roughness')] = cn_geometry['throat.epsilon']
             if 'throat.diameters' in cn_geometry:
@@ -75,5 +84,35 @@ class VtkDataExporter:
             
             filename = os.path.join(self.output_dir, f'output_timestep_{i:04d}.vtk')
             polydata.save(filename)
+
+    def export_static_node_markers(self, cn_geometry, inflow_nodes=None, outflow_nodes=None):
+        """
+        Exports a static VTK file with inflow and outflow node markers.
+
+        Args:
+            cn_geometry (openpnm.network): The network geometry.
+            inflow_nodes (list, optional): List of inflow node indices. Defaults to None.
+            outflow_nodes (list, optional): List of outflow node indices. Defaults to None.
+        """
+        points = cn_geometry['pore.coords']
+        polydata = pv.PolyData()
+        polydata.points = points
+
+        # Optionally add inflow nodes
+        if inflow_nodes is not None:
+            inflow_array = np.zeros(points.shape[0], dtype=int)
+            inflow_array[inflow_nodes] = 1
+            polydata.point_data[self.vtk_safe('InflowNodes')] = inflow_array
+
+        # Optionally add outflow nodes
+        if outflow_nodes is not None:
+            outflow_array = np.zeros(points.shape[0], dtype=int)
+            outflow_array[outflow_nodes] = 1
+            polydata.point_data[self.vtk_safe('OutflowNodes')] = outflow_array
+        
+        # Save the static node markers to a VTK file
+        static_filename = os.path.join(self.output_dir, 'static_node_markers.vtk')
+        polydata.save(static_filename)
+
             
           
